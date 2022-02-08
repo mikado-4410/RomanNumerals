d7cb0f33054dc6c015ed719df3968c4c87ff9efc
mikado-4410 <yosito72golf@gmail.com>
2022-02-08 22:09:34 +0900

    update:Add python version

--- python/main.py
+++ python/main.py
@@ -1,14 +1,19 @@
 import sys
-from package.converter  
+from package.converter import RomanNumeralConverter
 
 
 if __name__ == '__main__':
     args = sys.argv
+    Converter = RomanNumeralConverter()
     if len(args) == 1:
         print('No integer is given as the execution parameter.')
+        Converter.AddArabianNumber()
 
     for i in range(len(args)):
         if i == 0:
             continue
 
-        print(args[i])
+        Converter.input_number = int(args[i])
+        Converter.CheckInputNumber()
+
+    sys.exit()
--- python/package/converter.py
+++ python/package/converter.py
@@ -2,8 +2,8 @@ import sys
 
 
 class RomanNumeralConverter:
-    MIN_ROMAN_NUMERAL = 1
-    MAX_ROMAN_NUMERAL = 3999
+    MIN_ROMAN_NUMERAL = int(1)
+    MAX_ROMAN_NUMERAL = int(3999)
 
     input_number = 0
 
@@ -12,14 +12,29 @@ class RomanNumeralConverter:
             print('{} is not proper input.'.format(self.input_number))
             self.AddArabianNumber()
 
+        self.ApplyRomanConverter()
+
     def AddArabianNumber(self):
         print(
-            'Please enter the integer again in the range of 1 to 3999 (Enter 0 to exit).\n')
+            'Please enter the integer again in the range of 1 to 3999 (Enter 0 to exit).')
         self.input_number = input('Please enter a value -> ')
         if self.input_number:
             self.CheckInputNumber()
         else:
             sys.exit()
-    
+
     def ApplyRomanConverter(self):
-        i = 0
+        self.i = 0
+        self.romanNumber = ''
+        self.correspondingValue = (
+            (100, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+            (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        )
+        while self.input_number:
+            while (self.input_number / self.correspondingValue[self.i][0]):
+                self.romanNumber += self.correspondingValue[self.i][1]
+                self.input_number -= self.correspondingValue[self.i][0]
+
+            self.i += 1
+
+        print(self.romanNumber)
