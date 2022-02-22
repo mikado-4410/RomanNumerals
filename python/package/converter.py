5942063b7f70e13da3f061ee09b36e32e5ca3e68
mikado-4410 <yosito72golf@gmail.com>
2022-02-22 11:48:16 +0900

    Fix: Location of the code

--- python/package/converter.py
+++ python/package/converter.py
@@ -28,7 +28,8 @@ class RomanNumeralConverter:
         self.romanNumber = ''
         self.correspondingValue = (
             (100, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
-            (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+            (50, 'L'), (40, 'XL'), (10, 'X'), (9,
+                                               'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
         )
         while self.input_number:
             while (self.input_number / self.correspondingValue[self.i][0]):
